e57b5f46cd1c35fd23fe8a7d0d1467734d4757f3
snsepton <sonja.septon@utah.edu>
2021-11-08 16:23:34 -0700

    for_loops

---
+++ index.js
@@ -0,0 +1,12 @@
+for(var i = 0; i < 101; i++){
+    if (i === 0) {
+        console.log(i +  " is even");
+}
+else if (i % 2 === 0) {
+        console.log(i + " is even");   
+}
+else {
+        console.log(i + " is odd");
+}
+    console.log(i)
+}
